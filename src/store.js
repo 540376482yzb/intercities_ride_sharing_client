fa66a95a72897e822ebc481a2dab22a9a5b983c2
Zhou <540376482yzb@gmail.com>
2018-05-30 01:57:30 -0700

    socket

--- src/store.js
+++ src/store.js
@@ -1,16 +1,16 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import {createStore, applyMiddleware, combineReducers} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
-import { reducer as formReducer } from 'redux-form'
+import {reducer as formReducer} from 'redux-form'
 import rideReducer from './reducers/rides'
 import authReducer from './reducers/auth'
 import locationReducer from './reducers/location'
-import { setAuthToken, refreshAuthToken } from './actions/auth'
-import { loadAuthToken } from './local-storage'
+import {setAuthToken, refreshAuthToken} from './actions/auth'
+import {loadAuthToken} from './local-storage'
 import socketReducer from './reducers/socket'
 import control from './reducers/control'
 import io from 'socket.io-client'
-import { initializeSocket } from './actions/socket'
+import {initializeSocket} from './actions/socket'
 export const store = createStore(
 	combineReducers({
 		rideReducer,
@@ -23,12 +23,10 @@ export const store = createStore(
 	composeWithDevTools(applyMiddleware(thunk))
 )
 
-const socket = io('localhost:8080')
 //persist autoToken into local storage
 const authToken = loadAuthToken()
 if (authToken) {
 	store.dispatch(setAuthToken(authToken))
 	store.dispatch(refreshAuthToken())
-	store.dispatch(initializeSocket(socket))
 }
 export default store
